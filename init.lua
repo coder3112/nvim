68a8769fd1c93e01d8bd955a82fe505863bba87b
Naitik Mundra <mundranaitik@outlook.com>
2024-02-21 10:09:10 +0100

    Put most basic stuff in

--- init.lua
+++ init.lua
@@ -9,5 +9,172 @@ if not vim.loop.fs_stat(lazypath) then
     "--branch=stable", -- latest stable release
     lazypath,
   })
+end
 
 vim.opt.rtp:prepend(lazypath)
+
+-- Keymaps
+vim.g.mapleader = " " --Spacebar is leader
+
+
+-- Install important plugins
+require("lazy").setup({
+	{"ellisonleao/gruvbox.nvim", priority=1000, config=true, opts=...},
+	{"mrcjkb/rustaceanvim", version="^4", ft={'rust', 'rs'}},
+	{'echasnovski/mini.nvim'},
+	{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+},
+	"nvim-treesitter/nvim-treesitter",
+	"mfussenegger/nvim-dap",
+	"williamboman/mason.nvim",
+	"neovim/nvim-lspconfig",
+	"williamboman/mason-lspconfig.nvim",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	'hrsh7th/cmp-nvim-lua',
+    	'hrsh7th/cmp-nvim-lsp-signature-help',
+    	'hrsh7th/cmp-vsnip',          
+    	'hrsh7th/cmp-path',                              
+    	'hrsh7th/cmp-buffer',                            
+    	'hrsh7th/vim-vsnip',
+	'puremourning/vimspector'
+})
+
+
+-- UI
+vim.o.background ="dark"
+vim.wo.relativenumber = true
+vim.wo.number = true
+vim.cmd[[colorscheme gruvbox]]
+
+--LSP
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
+
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.cmd[[autocmd BufWinEnter * let &foldlevel = max(map(range(1, line('$')), 'foldlevel(v:val)'))]] --unfold everything when a file is opened
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+local cmp = require'cmp'
+cmp.setup({
+  -- Enable LSP snippets
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    })
+  },
+  -- Installed sources:
+  sources = {
+    { name = 'path' },                              -- file paths
+    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
+    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
+    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
+    { name = 'buffer', keyword_length = 2 },        -- source current buffer
+    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+    { name = 'calc'},                               -- source for math calculation
+  },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+      fields = {'menu', 'abbr', 'kind'},
+      format = function(entry, item)
+          local menu_icon ={
+              nvim_lsp = 'λ',
+              vsnip = '⋗',
+              buffer = 'Ω',
+              path = '🖫',
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+      end,
+  },
+})
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "lua", "rust", "toml" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting=false,
+  },
+  ident = { enable = true }, 
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
+
+
+vim.cmd([[
+let g:vimspector_sidebar_width = 85
+let g:vimspector_bottombar_height = 15
+let g:vimspector_terminal_maxwidth = 70
+]])
+vim.cmd([[
+nmap <F9> <cmd>call vimspector#Launch()<cr>
+nmap <F5> <cmd>call vimspector#StepOver()<cr>
+nmap <F8> <cmd>call vimspector#Reset()<cr>
+nmap <F11> <cmd>call vimspector#StepOver()<cr>")
+nmap <F12> <cmd>call vimspector#StepOut()<cr>")
+nmap <F10> <cmd>call vimspector#StepInto()<cr>")
+map('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>")
+map('n', "Dw", ":call vimspector#AddWatch()<cr>")
+map('n', "De", ":call vimspector#Evaluate()<cr>")
+]])
+
+
+-- mini.nvim
+require("mini.ai").setup()
+require("mini.operators").setup()
+require("mini.surround").setup()
+require("mini.bracketed").setup()
+require("mini.files").setup()
+require("mini.comment").setup()
+require("mini.pairs").setup()
+require("mini.statusline").setup()
+
