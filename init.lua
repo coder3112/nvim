8f2903396fd01f24f8cf0aad16e122ed19f2ac2d
Naitik Mundra <mundranaitik@outlook.com>
2024-03-23 19:56:15 +0100

    todo; line numbers

---
+++ init.lua
@@ -0,0 +1,34 @@
+-- This is my ultimate config.
+--
+-- TODO: Package Manager
+-- TODO: Treesitter
+-- TODO: LSP
+-- TODO: Snippets
+-- TODO: Project Management 
+-- 	- new files
+-- 	- fzf
+-- 	- compile
+-- 	- run
+-- 	- test
+--
+
+
+--UI
+
+-- Set line numbers. Current line is absolute, others are relativ
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
+
+--Language
+--
+
+
+--Project Management
+--
+
+
+-- Keybinds
+--TODO: convinience functions to port map, nmap
+
