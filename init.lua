308611243d625adf994ffb6aceded106cf6ca4ea
Naitik Mundra <mundranaitik@outlook.com>
2024-03-24 09:05:17 +0100

    coming along

--- init.lua
+++ init.lua
@@ -30,5 +30,18 @@ vim.opt.signcolumn = "number"
 
 
 -- Keybinds
---TODO: convinience functions to port map, nmap
 
+--Convinience functions to port map, nmap
+function map(mode, shortcut, command)
+  vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+end
+
+function nmap(shortcut, command)
+  map('n', shortcut, command)
+end
+
+function imap(shortcut, command)
+  map('i', shortcut, command)
+end
+
+nmap(<C-s>, :w<CR>) --Ctrl+S to save
