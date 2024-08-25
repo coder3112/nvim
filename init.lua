6163e7c6756f1661e9f39876a8da3408ed2f8c3a
Naitik Mundra <mundranaitik@outlook.com>
2024-08-25 23:36:10 +0200

    Oil is nice

--- init.lua
+++ init.lua
@@ -13,15 +13,30 @@
 -- 	- test
 --	- git
 
+vim.o.expandtab = true -- expand tab input with spaces characters
+vim.o.smartindent = true -- syntax aware indentations for newline inserts
+vim.o.tabstop = 2 -- num of space characters per tab
+vim.o.shiftwidth = 2 -- spaces per indentation level
+
 require "paq" {
     "savq/paq-nvim", -- Let Paq manage itself
+    {"nvim-treesitter/nvim-treesitter", build=':TSUpdate'}, -- TS is the best syntax highlighter there is.
+    "stevearc/conform.nvim", --formatter
+    "windwp/nvim-autopairs", -- close pairs of brackets,
+    "stevearc/oil.nvim", -- file explorer that behaves like a neovim buffer.
+
 }
 
 
+-- File Explorer: Oil
+require("oil").setup()
+
 --UI
+vim.cmd[[colorscheme torte]]
 
 vim.opt.splitbelow = true --:split opens buffer below. very useful for terminal stuff.
 vim.opt.splitright = true --:vsplit open buffer on right. more intuitive to me.
+vim.opt.mouse = "a" --convenient mouse
 
 -- Set line numbers. Current line is absolute, others are relative
 vim.opt.number = true
@@ -30,7 +45,19 @@ vim.opt.signcolumn = "number"
 
 
 --Language
---
+
+--Autoformating
+require("conform").setup({
+	formatters_by_ft = {
+		lua = {"styllua"},
+		rust = {"rustfmt", lsp_format = "fallback"}
+	}
+})
+
+-- Autopairs
+require('nvim-autopairs').setup({
+  disable_filetype = { "TelescopePrompt" , "vim" },
+})
 
 
 --Project Management
