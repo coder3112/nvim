e4642946642ebf51b61c343e387b5aee1e978ed9
Naitik Mundra <mundranaitik@outlook.com>
2024-08-25 22:53:08 +0200

    Actually working on a minimal and fast config now. NO COPY PASTING

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
---This is my ultimate config.
+-- This is my ultimate config.
 --
--- TODO: Package Manager
--- Langauges: Haskell, Rust, LaTeX, Typst?, python
+-- DONE: Package Manager
 -- TODO: Treesitter
+-- TODO: Highlight TODO/DONE/FIX/BUG etc. Maybe come up with simple shell script to output/pretty print etc.
 -- TODO: LSP
 -- TODO: Snippets
 -- TODO: Project Management 
@@ -11,162 +11,27 @@
 -- 	- compile
 -- 	- run
 -- 	- test
--- TODO: Note Taking
--- TODO: Git. 
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+--	- git
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+require "paq" {
+    "savq/paq-nvim", -- Let Paq manage itself
+}
 
-require("lazy").setup({
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-  }, -- Autopair Brackets
-  "neovim/nvim-lspconfig", -- Vanilla LSP
-  'hrsh7th/nvim-cmp', -- Autocompletion plugin
-  'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
-  'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
-  'L3MON4D3/LuaSnip', -- Snippets plugin,
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "mfussenegger/nvim-dap",
-  { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
-  "mfussenegger/nvim-lint",
-  'mhartington/formatter.nvim'
-})
 
 --UI
--- Set line numbers. Current line is absolute, others are relativ
+
+-- Set line numbers. Current line is absolute, others are relative
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 
-vim.cmd 'colorscheme torte' -- colorscheme
+
+--Language
+--
 
 
 --Project Management
 --
---
-
--- Langauges/LSP
--- require("mason").setup()
-require("mason").setup()
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local lspconfig = require('lspconfig')
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'hls', 'texlab'}
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    capabilities = capabilities,
-  }
-end
-local luasnip = require 'luasnip'
 
 
 -- Keybinds
-
---Convinience functions to port map, nmap
-function map(mode, shortcut, command)
-  vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
-end
-
-function nmap(shortcut, command)
-  map('n', shortcut, command)
-end
-
-function imap(shortcut, command)
-  map('i', shortcut, command)
-end
-
-nmap("<C-s>", ":w<CR>") --Ctrl+S to save
-imap("<C-s>", "<Esc>:w<CR>") --Ctrl+S to save
-
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-augroup("__formatter__", { clear = true })
-autocmd("BufWritePost", {
-	group = "__formatter__",
-	command = ":FormatWrite",
-})
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
-})
-
-local cmp = require 'cmp'
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-    -- C-b (back) C-f (forward) for snippet placeholder navigation.
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
